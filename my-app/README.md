e5f0c9aa45331cf7fb998c10a528506131f458bc
gubiczam <gubiczamilan2004@gmail.com>
2025-09-26 23:54:53 +0200

    feat: initial create-zeon-app scaffolder

--- my-app/README.md
+++
@@ -1,1 +0,0 @@
-# create-zeon-app
--- my-app/main.ts
+++
@@ -1,9 +0,0 @@
-import { signal } from "@usezeon/core";
-import { h, mount } from "@usezeon/runtime";
-
-const count = signal(0);
-const view = h("div", {},
-  h("p", {}, () => "Count: " + count.get()),
-  h("button", { onClick: () => count.set(count.get()+1) }, "Inc")
-);
-mount(document.getElementById("app")!, view);
